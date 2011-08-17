4b08719205f648d4a7ff9c5d0670711b58e24c3b
Tim Hütz <tim@huetz.biz>
2011-08-17 11:38:41 +0200

    Added a progress bar to show the current application status

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 	private Button buttonBackupInstalledApplications = null;
 	private Button buttonRestoreInstalledApplications = null;
+	private ProgressBar progressBarCurrentStatus = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class MainActivity extends Activity {
         // get some control handles
         this.buttonBackupInstalledApplications = (Button)this.findViewById( R.id.buttonBackupInstalledApplications );
         this.buttonRestoreInstalledApplications = (Button)this.findViewById( R.id.buttonRestoreInstalledApplications );
+        this.progressBarCurrentStatus = (ProgressBar)this.findViewById( R.id.progressBarCurrentStatus );
         
         // add a click handler for the button to backup the installed applications
         this.buttonBackupInstalledApplications.setOnClickListener( new OnClickListener() {
