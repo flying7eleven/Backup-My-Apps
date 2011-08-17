daec914330b1b7774ac79e38f69779a16b8ea3c7
Tim Hütz <tim@huetz.biz>
2011-08-17 11:26:43 +0200

    Added some initial button click handlers

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -2,6 +2,8 @@ package com.halcyonwaves.apps.backupmyapps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -17,5 +19,19 @@ public class MainActivity extends Activity {
         // get some control handles
         this.buttonBackupInstalledApplications = (Button)this.findViewById( R.id.buttonBackupInstalledApplications );
         this.buttonRestoreInstalledApplications = (Button)this.findViewById( R.id.buttonRestoreInstalledApplications );
+        
+        // add a click handler for the button to backup the installed applications
+        this.buttonBackupInstalledApplications.setOnClickListener( new OnClickListener() {
+			public void onClick(View v) {
+				// TODO this
+			}
+		});
+        
+        // add a click handler for the button to restore the applications
+        this.buttonRestoreInstalledApplications.setOnClickListener( new OnClickListener() {
+			public void onClick(View v) {
+				// TODO this
+			}
+		});
     }
 }
