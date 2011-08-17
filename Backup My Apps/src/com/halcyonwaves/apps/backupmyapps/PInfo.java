f65f6d0b03b249cc6bba7896061e84d8af27bd7e
Tim Hütz <tim@huetz.biz>
2011-08-17 16:38:31 +0200

    Added a new helper class

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/PInfo.java
@@ -0,0 +1,16 @@
+package com.halcyonwaves.apps.backupmyapps;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+public class PInfo {
+    private String appname = "";
+    private String pname = "";
+    private String versionName = "";
+    private int versionCode = 0;
+    private Drawable icon;
+    
+    private void prettyPrint() {
+        Log.v(appname + "\t" + pname + "\t" + versionName + "\t" + versionCode);
+    }
+}
