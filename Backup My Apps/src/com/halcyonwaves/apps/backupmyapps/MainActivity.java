cdc589b4cd0df85be24e1429ec71e864b4ae0c56
Tim Hütz <tim@huetz.biz>
2011-08-17 11:24:24 +0200

    Updated the code of the main activity to get the handles to the two buttons

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -2,12 +2,20 @@ package com.halcyonwaves.apps.backupmyapps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+	private Button buttonBackupInstalledApplications = null;
+	private Button buttonRestoreInstalledApplications = null;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	// create the layout of the main activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // get some control handles
+        this.buttonBackupInstalledApplications = (Button)this.findViewById( R.id.buttonBackupInstalledApplications );
+        this.buttonRestoreInstalledApplications = (Button)this.findViewById( R.id.buttonRestoreInstalledApplications );
     }
 }
