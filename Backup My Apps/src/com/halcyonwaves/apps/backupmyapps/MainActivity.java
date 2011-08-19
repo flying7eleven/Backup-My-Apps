858d64c112d3a9277a3066d996962f12ca779b9b
Tim Hütz <tim@huetz.biz>
2011-08-19 09:30:18 +0200

    Done a bit UI cleanup

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -15,8 +15,7 @@ import android.widget.ProgressBar;
 public class MainActivity extends Activity {
 	private Button buttonBackupInstalledApplications = null;
 	private Button buttonRestoreInstalledApplications = null;
-	private ProgressBar progressBarCurrentStatus = null;
-	
+		
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	// create the layout of the main activity
@@ -26,7 +25,11 @@ public class MainActivity extends Activity {
         // get some control handles
         this.buttonBackupInstalledApplications = (Button)this.findViewById( R.id.buttonBackupInstalledApplications );
         this.buttonRestoreInstalledApplications = (Button)this.findViewById( R.id.buttonRestoreInstalledApplications );
-        this.progressBarCurrentStatus = (ProgressBar)this.findViewById( R.id.progressBarCurrentStatus );
+        
+        // if there is no backup file, disable the restore button
+        if( true ) { // TODO: this
+        	this.buttonRestoreInstalledApplications.setEnabled( false );
+        }
         
         // add a click handler for the button to backup the installed applications
         this.buttonBackupInstalledApplications.setOnClickListener( new OnClickListener() {
