eefbce1c6d663328234261ae1b12b6f904ffe4ba
Tim Hütz <tim@huetz.biz>
2011-08-21 17:07:41 +0200

    Added a class for doing a async task

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/GatherBackupInformationTask.java
@@ -0,0 +1,19 @@
+package com.halcyonwaves.apps.backupmyapps;
+
+import android.os.AsyncTask;
+
+/**
+ * Class for gathering the backup information in an async way.
+ * 
+ * @author Tim Huetz
+ * @since 0.2
+ */
+public class GatherBackupInformationTask extends AsyncTask< Void, Void, Integer > {
+
+	@Override
+	protected Integer doInBackground( Void... arg0 ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
