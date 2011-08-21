3d9b215c8737068bc836e9cc14adb87c3a7627bc
Tim Hütz <tim@huetz.biz>
2011-08-21 14:30:14 +0200

    Removed some not-required import statements

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -11,7 +11,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
@@ -22,7 +21,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
