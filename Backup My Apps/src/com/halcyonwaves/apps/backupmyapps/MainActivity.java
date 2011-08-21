728070b3860101553e919aea943ed1672f7256dc
Tim Hütz <tim@huetz.biz>
2011-08-21 17:19:38 +0200

    Moved the lines for creating the backup to the external backup class which can be executed in a async. way

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -1,11 +1,6 @@
 package com.halcyonwaves.apps.backupmyapps;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -30,7 +25,6 @@ public class MainActivity extends Activity {
 	private TextView textViewAdditionalInformation = null;
 	private Dialog dialogHelp = null;
 	private Dialog dialogAbout = null;
-	private PackageInformationManager packageInformationManager = null;
 	private ProgressDialog backupProgressDialog = null;
 	private static final String BACKUP_FILENAME = "installedApplications.backupmyapps";
 	private final File storagePath = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_DOWNLOADS );
@@ -59,53 +53,15 @@ public class MainActivity extends Activity {
 		// be sure that the storage path exists
 		this.storagePath.mkdirs();
 
-		// get the package manager for this activity
-		this.packageInformationManager = new PackageInformationManager( this );
-
 		// add a click handler for the button to backup the installed applications
 		this.buttonBackupInstalledApplications.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
-				// just log some information
-				Log.v( MainActivity.class.getSimpleName(), "Using following external storage directory: " + MainActivity.this.storagePath );
-
 				// show a progress dialog
 				MainActivity.this.backupProgressDialog = ProgressDialog.show( MainActivity.this, "", MainActivity.this.getString( R.string.progressDialogBackupInProgress ), true );
-
-				// try to open the output file
-				File backupFile = new File( MainActivity.this.storagePath, MainActivity.BACKUP_FILENAME );
-				try {
-					backupFile.createNewFile();
-					OutputStream backupFileStream = new FileOutputStream( backupFile );
-					PrintStream backupFilePrintStream = new PrintStream( backupFileStream );
-
-					// write the XML meta information
-					backupFilePrintStream.print( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" );
-					backupFilePrintStream.print( "<BackupMyApps version=\"1.0\">" );
-
-					// loop through the found packages and write them into the file
-					ArrayList< PackageInformation > foundPackages = MainActivity.this.packageInformationManager.getInstalledApps();
-					for( PackageInformation currentPackage : foundPackages ) {
-						if( !currentPackage.isSystemComponent() ) {
-							backupFilePrintStream.print( "<InstalledApp packageName=\"" + currentPackage.getPackageName() + "\" " );
-							backupFilePrintStream.print( "humanReadableName=\"" + currentPackage.getApplicationName().replace( "&", "&amp;" ).replace( "'", "&apos;" ).replace( "\"", "&quot;" ).replace( "<", "&lt;" ).replace( ">", "&gt;" ) + "\" " );
-							backupFilePrintStream.print( "versionCode=\"" + currentPackage.getVersionCode() + "\" " );
-							backupFilePrintStream.print( "versionName=\"" + currentPackage.getVersionName() + "\"" );
-							backupFilePrintStream.print( "/>" );
-						}
-					}
-
-					// write the closing tags and close the stream
-					backupFilePrintStream.print( "</BackupMyApps>" );
-					backupFileStream.close();
-
-					// as we succeeded in writing the file, we can enable the restore button now and
-					// disable the
-					// progress dialog
-					MainActivity.this.buttonRestoreInstalledApplications.setEnabled( true );
-					//MainActivity.this.backupProgressDialog.dismiss();
-				} catch( IOException e ) {
-					Log.e( MainActivity.class.getSimpleName(), "Failed to create the backup file. The message was: " + e.getMessage() );
-				}
+				
+				// create and execute the backup task
+				GatherBackupInformationTask gatherTask = new GatherBackupInformationTask( MainActivity.this, MainActivity.this.storagePath, MainActivity.BACKUP_FILENAME );
+				gatherTask.execute();
 			}
 		} );
 
