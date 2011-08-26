58b6c8e8720318d2415e777dd9d92a3fc214cd61
Tim Hütz <tim@huetz.biz>
2011-08-26 10:16:00 +0200

    Added a new class for restoring a backup

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RetoreBackupDataTask.java
@@ -0,0 +1,13 @@
+package com.halcyonwaves.apps.backupmyapps;
+
+import android.os.AsyncTask;
+
+public class RetoreBackupDataTask extends AsyncTask< Void, Void, Boolean > {
+
+	@Override
+	protected Boolean doInBackground( Void... arg0 ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
