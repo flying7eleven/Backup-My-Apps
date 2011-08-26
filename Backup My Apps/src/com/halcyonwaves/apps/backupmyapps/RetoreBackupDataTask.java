6c083d11581567459f71ca9e220a63d898d1873e
Tim Hütz <tim@huetz.biz>
2011-08-26 10:19:03 +0200

    Implemented some required methods in the restore task

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RetoreBackupDataTask.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RetoreBackupDataTask.java
@@ -1,9 +1,39 @@
 package com.halcyonwaves.apps.backupmyapps;
 
+import java.io.File;
+
+import android.content.Context;
 import android.os.AsyncTask;
 
+/**
+ * Class for restoring backed-up applications in an asynchronous way.
+ * 
+ * @author Tim Huetz
+ * @since 0.3
+ */
 public class RetoreBackupDataTask extends AsyncTask< Void, Void, Boolean > {
-
+	private File storagePath = null;
+	private Context applicationContext = null;
+	private String backupFilename = "";
+	private IAsyncTaskFeedback feedbackClass = null;
+	
+	/**
+	 * Constructor for this class.
+	 * 
+	 * @param applicationContext The context of the application.
+	 * @param storagePath The path to the backup file.
+	 * @param backupFilename The name of the backup file.
+	 * @param feedbackClass The class which should handle the feedback of this task.
+	 * @author Tim Huetz
+	 * @since 0.3
+	 */
+	public RetoreBackupDataTask( Context applicationContext, File storagePath, String backupFilename, IAsyncTaskFeedback feedbackClass ) {
+		this.storagePath = storagePath;
+		this.applicationContext = applicationContext;
+		this.backupFilename = backupFilename;
+		this.feedbackClass = feedbackClass;
+	}
+	
 	@Override
 	protected Boolean doInBackground( Void... arg0 ) {
 		// TODO Auto-generated method stub
