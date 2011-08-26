d626d9e3fe463dd465385ca0c1759adf0a5d1c51
Tim Hütz <tim@huetz.biz>
2011-08-26 10:07:31 +0200

    Added mising class documentation

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/controls/BackupListActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/controls/BackupListActivity.java
@@ -3,6 +3,9 @@ package com.halcyonwaves.apps.backupmyapps.controls;
 import android.app.ListActivity;
 
 /**
+ * This class implements a new activity type which is used to display
+ * the list of backed-up applications. In this list the user can select
+ * the applications which shlould be restored and which not.
  * 
  * @author Tim HŸtz
  * @since 0.4
