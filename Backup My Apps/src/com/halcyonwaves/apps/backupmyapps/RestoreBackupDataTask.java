54c9af1af59ffcf68305758ba128bf04eceefb0d
Tim Hütz <tim@huetz.biz>
2011-08-27 16:26:04 +0200

    The application can now parse the backup file

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreBackupDataTask.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreBackupDataTask.java
@@ -1,8 +1,13 @@
 package com.halcyonwaves.apps.backupmyapps;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
@@ -61,7 +66,7 @@ public class RestoreBackupDataTask extends AsyncTask< Void, Void, Boolean > {
 	private Document XMLfromString( String xml ) {
 		Document doc = null;
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		
+
 		try {
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			InputSource is = new InputSource();
@@ -88,7 +93,30 @@ public class RestoreBackupDataTask extends AsyncTask< Void, Void, Boolean > {
 		Log.v( RestoreBackupDataTask.class.getSimpleName(), "Using following external storage directory: " + this.storagePath );
 
 		// try to open the input file
-		File backupFile = new File( this.storagePath, this.backupFilename );
+		try {
+			// get the input stream to the backup file
+			InputStream backupFileStream = new FileInputStream( new File( this.storagePath, this.backupFilename ) );
+
+			// prepare the file for reading
+			InputStreamReader inputreader = new InputStreamReader( backupFileStream );
+			BufferedReader buffreader = new BufferedReader( inputreader );
+
+			// read the whole file into the memory
+			String line, full = "";
+			while( (line = buffreader.readLine()) != null ) { // TODO: optimize this
+				full += line;
+			}
+			
+			// get the xml document from the string
+			Document backupFile = this.XMLfromString( full );
+
+		} catch( FileNotFoundException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch( IOException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		//
 		// Intent intent = new Intent(Intent.ACTION_VIEW);
