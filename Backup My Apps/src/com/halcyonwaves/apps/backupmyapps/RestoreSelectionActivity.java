fef68d7f79e9d283739fdf61bec4ba5f7d7af53d
Tim Hütz <tim@huetz.biz>
2011-09-01 15:44:15 +0200

    Added a sample implementation of a list

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreSelectionActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreSelectionActivity.java
@@ -1,6 +1,9 @@
 package com.halcyonwaves.apps.backupmyapps;
 
 import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 /**
  * 
@@ -8,5 +11,13 @@ import android.app.ListActivity;
  * @since 0.3
  */
 public class RestoreSelectionActivity extends ListActivity {
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		// Create some mock data
+		String[] testValues = new String[]{ "Test1", "Test2", "Test3", "Test4" };
 
+		ListAdapter adapter = new ArrayAdapter< String >( this, android.R.layout.simple_list_item_1, testValues );
+		this.setListAdapter( adapter );
+	}
 }
