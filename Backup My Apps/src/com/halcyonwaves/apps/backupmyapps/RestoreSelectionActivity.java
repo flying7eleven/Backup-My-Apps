6134ca63134d78bd76080e9a1f00bf6a70329ba3
Tim Hütz <tim@huetz.biz>
2011-09-01 16:14:27 +0200

    Done a little bit code cleanup

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreSelectionActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreSelectionActivity.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -37,4 +39,37 @@ public class RestoreSelectionActivity extends ListActivity {
 		ListAdapter adapter = new ArrayAdapter< String >( this, android.R.layout.simple_list_item_1, (String[])foundPackages.toArray(new String[0]) );
 		this.setListAdapter( adapter );
 	}
+	
+	private void installPackage(String pathToApk) { // see http://www.anddev.org/androidpermissioninstall_packages_not_granted-t5858.html for more
+		// see http://stackoverflow.com/questions/5803999/install-apps-silently-with-granted-install-packages-permission
+		//Uri packageUri = Uri.fromFile( new File( pathToApk ) );
+		// set permissions
+		//PackageManager pm = this.applicationContext.getPackageManager();
+		// pm.addPermission(p);//error
+		// pm.installPackage(packageUri, null, 1);
+	}
+	
+	/**
+	 * Try to open the market with the supplied package name. If this fails, open
+	 * the browser URL of the market and search the package there.
+	 * 
+	 * @param packageName The name of the package to install.
+	 */
+	private void installPackageFromMarket( String packageName ) {
+		try {
+			Intent browserIntent = new Intent( Intent.ACTION_VIEW, Uri.parse( "market://details?id=" + packageName ) );
+			this.startActivity( browserIntent );
+		} catch( Exception outerException ) {
+			Log.e( RestoreBackupDataTask.class.getSimpleName(), "Failed to open the market directly. The exception was: " + outerException.getMessage() );
+			try {
+				Log.v( RestoreBackupDataTask.class.getSimpleName(), "Opening browser directly!!" );
+				Intent browserIntent = new Intent( Intent.ACTION_VIEW, Uri.parse( "https://market.android.com/details?id=" + packageName ) );
+				this.startActivity( browserIntent );
+				Log.v( RestoreBackupDataTask.class.getSimpleName(), "Browser closed!" );
+			} catch( Exception innerException ) {
+				Log.e( RestoreBackupDataTask.class.getSimpleName(), "Failed to open the market in the browser. The exception was: " + innerException.getMessage() );
+			}
+		}
+	}
+
 }
