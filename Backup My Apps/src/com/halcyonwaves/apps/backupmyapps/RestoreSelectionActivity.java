fcb2193928b37e5bbc806bf77c5b7f7fabb3c39e
Tim Hütz <tim@huetz.biz>
2011-09-01 15:30:23 +0200

    Started to implement a class for the list activity for the restore process

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/RestoreSelectionActivity.java
@@ -0,0 +1,12 @@
+package com.halcyonwaves.apps.backupmyapps;
+
+import android.app.ListActivity;
+
+/**
+ * 
+ * @author Tim Huetz
+ * @since 0.3
+ */
+public class RestoreSelectionActivity extends ListActivity {
+
+}
