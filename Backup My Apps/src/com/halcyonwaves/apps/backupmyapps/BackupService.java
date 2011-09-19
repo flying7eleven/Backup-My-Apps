675da901f0ead72965607f24580e1b469a271deb
Tim Hütz <tim@huetz.biz>
2011-09-19 19:39:02 +0200

    Started to implement the background service

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/BackupService.java
@@ -0,0 +1,31 @@
+package com.halcyonwaves.apps.backupmyapps;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * This class implements the background service which does the automatic
+ * backup of the installed applications.
+ * 
+ * @author Tim Huetz
+ * @since 0.4
+ */
+public class BackupService extends IntentService {
+
+	/**
+	 * Default constructor of this class.
+	 * 
+	 * @author Tim Huetz
+	 * @since 0.4
+	 */
+	public BackupService() {
+		super( "BackupMyAppsBackupService" );
+	}
+	
+	@Override
+	protected void onHandleIntent( Intent arg0 ) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
