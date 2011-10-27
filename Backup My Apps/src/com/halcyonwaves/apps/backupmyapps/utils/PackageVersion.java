98e5a9a85a63627a7decefe432931f23c782b515
Tim Hütz <tim@huetz.biz>
2011-10-27 16:38:30 +0200

    Started to implement a new class to parsing version numbers

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/utils/PackageVersion.java
@@ -0,0 +1,11 @@
+package com.halcyonwaves.apps.backupmyapps.utils;
+
+/**
+ * This class implements some functions to parse a version string and compare the 
+ * version to other versions.
+ * 
+ * @author Tim Huetz
+ * @since 0.6
+ */
+public class PackageVersion {
+}
