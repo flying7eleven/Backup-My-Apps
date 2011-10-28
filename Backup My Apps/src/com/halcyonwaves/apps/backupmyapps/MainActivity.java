70783bde52a2b9825b93d2b8a0ccdadb05e788ac
Tim Hütz <tim@huetz.biz>
2011-10-28 09:55:21 +0200

    Modified the name of the backup file again

--- Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/MainActivity.java
@@ -211,7 +211,8 @@ public class MainActivity extends Activity implements IAsyncTaskFeedback {
 			// if we should sync, copy the file to the Dropbox account
 			if( true ) { // TODO check if we should do this
 				// define the backup filename
-				String backupFilename = "latest_backups.backupmyapps"; //TODO: android.os.Build.DEVICE + "-" + android.os.Build.MODEL + ".backupmyapps";
+				String backupFilename = android.os.Build.DEVICE + "-" + android.os.Build.MODEL + ".backupmyapps";
+				backupFilename = backupFilename.replace( ' ', '_' );
 
 				// upload the file
 				File backupFile = new File( this.storagePath, MainActivity.BACKUP_FILENAME );
