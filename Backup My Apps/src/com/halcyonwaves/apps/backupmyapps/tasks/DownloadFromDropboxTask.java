c1dbbc1dcb043792ca7319f1f372d172914e1bda
Tim Hütz <tim@huetz.biz>
2011-11-01 11:56:38 +0100

    Added two initial classes for doing the upload and download of the backupfiles into / from the Dropbox account

---
+++ Backup My Apps/src/com/halcyonwaves/apps/backupmyapps/tasks/DownloadFromDropboxTask.java
@@ -0,0 +1,19 @@
+package com.halcyonwaves.apps.backupmyapps.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Class for downloading the backup file from the Dropbox account.
+ * 
+ * @author Tim Huetz
+ * @since 0.5
+ */
+public class DownloadFromDropboxTask  extends AsyncTask< Void, Void, Boolean > {
+
+	@Override
+	protected Boolean doInBackground( Void... arg0 ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
