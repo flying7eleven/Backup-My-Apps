5814c05965abffba013f9f6762a2fb99f8920fb9
Tim Hütz <tim@huetz.biz>
2012-04-10 12:56:28 +0200

    Added a simple startup layout for the application

--- src/com/halcyonwaves/apps/backupmyapps/StartupActivity.java
+++ src/com/halcyonwaves/apps/backupmyapps/StartupActivity.java
@@ -1,6 +1,14 @@
 package com.halcyonwaves.apps.backupmyapps;
 
+import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class StartupActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate( final Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.setContentView( R.layout.startup_layout );
+	}
 }
